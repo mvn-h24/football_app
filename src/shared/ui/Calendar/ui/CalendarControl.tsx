c380db3fc829666894cc24a07b625f8bdcf04d96
mvn-h24 <morning.vecher.night@yandex.ru>
2024-05-12 07:50:25 +0300

    refactor(shared-ui): выделяет иконки

--- src/shared/ui/Calendar/ui/CalendarControl.tsx
+++ src/shared/ui/Calendar/ui/CalendarControl.tsx
@@ -1,3 +1,6 @@
+import NextIcon from "../icons/NextIcon.svg";
+import PrevIcon from "../icons/PrevIcon.svg";
+
 export interface CalendarControlProps {
   date: Date;
   prevMonth(): void;
@@ -15,49 +18,10 @@ export function CalendarControl({
       </span>
       <div className="buttons">
         <button className="p-1" onClick={prevMonth}>
-          <svg
-            width="1em"
-            height="1em"
-            viewBox="0 0 16 16"
-            className="bi bi-arrow-left-circle hover:opacity-60"
-            fill="currentColor"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
-            />
-            <path
-              fillRule="evenodd"
-              d="M8.354 11.354a.5.5 0 0 0 0-.708L5.707 8l2.647-2.646a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708 0z"
-            />
-            <path
-              fillRule="evenodd"
-              d="M11.5 8a.5.5 0 0 0-.5-.5H6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 .5-.5z"
-            />
-          </svg>
+          <PrevIcon className="bi bi-arrow-left-circle hover:opacity-60 size-6" />
         </button>
         <button className="p-1" onClick={nextMonth}>
-          <svg
-            width="1em"
-            height="1em"
-            viewBox="0 0 16 16"
-            className="bi bi-arrow-right-circle hover:opacity-60"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
-            />
-            <path
-              fillRule="evenodd"
-              d="M7.646 11.354a.5.5 0 0 1 0-.708L10.293 8 7.646 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0z"
-            />
-            <path
-              fillRule="evenodd"
-              d="M4.5 8a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5z"
-            />
-          </svg>
+          <NextIcon className="bi bi-arrow-left-circle hover:opacity-60 size-6" />
         </button>
       </div>
     </div>
